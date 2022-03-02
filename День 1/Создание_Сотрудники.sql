bc4e33742c7b1dcb0deca22e12187f98bd920028
XJIEBYIIIEK <64709241+XJIEBYIIIEK@users.noreply.github.com>
2022-03-02 16:01:32 +0300

    Add files via upload

---
+++ День 1/Создание_Сотрудники.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Сотрудники
+(
+	КодСотрудника		int IDENTITY (1,1)	NOT NULL	PRIMARY KEY,
+	ФИО					varchar(30)		NOT NULL,
+	Логин				varchar(20)		NOT NULL,
+	Пароль				varchar(20)		NOT NULL,
+	ДатаВхода			date								
+);
